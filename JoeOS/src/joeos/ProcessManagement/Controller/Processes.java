2af7c63eba55533964a0dd2498ab8ee591219373
Joe Giunti <jg4229@gmail.com>
2015-10-01 12:17:15 -0400

    scheduling q

--- JoeOS/src/joeos/ProcessManagement/Controller/Processes.java
+++ JoeOS/src/joeos/ProcessManagement/Controller/Processes.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 Joe.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package joeos.ProcessManagement.Controller;
 
@@ -11,7 +29,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import joeos.ProcessManagement.Models.PCBlock;
 import joeos.ProcessManagement.Models.ProcessTable;
@@ -27,12 +44,12 @@ public class Processes {
     
     public static void intitialize() throws FileNotFoundException, IOException{       
         int globalTime = 0;
+        int cpuTime = 0;
         File procData = new File("G:/Documents/Computer Science/CS451/project2/processes2.txt");
         FileReader reader = new FileReader(procData);
         BufferedReader br = new BufferedReader(reader);
         LinkedList<VirtualProcess> procList = new LinkedList<>();
         String line = null;
-        int i = 0;
         while((line = br.readLine()) != null){
             VirtualProcess vp = new VirtualProcess(line.split("\\s"));
             procList.offer(vp);
@@ -40,7 +57,7 @@ public class Processes {
         ProcessTable pTable = new ProcessTable();  
         pTable.init();
         while(!procList.isEmpty()){
-            for(i = 0; i < procList.size(); i ++){
+            for(int i = 0; i < procList.size(); i ++){
                 if(procList.get(i).getArrivalTime() == globalTime){
                     VirtualProcess arrivedProcess = procList.get(i);
                     if(!pTable.isFull()){
@@ -62,8 +79,8 @@ public class Processes {
         
         ArrayList<PCBlock> ptable2 = pTable.getTable();
         ptable2.stream().forEach((processBlock) -> {
-            System.out.println("Process Name: " + processBlock.pName + "\n" +
-                    "Process   ID: " + processBlock.pID);
+            System.out.println("Process Name: " + processBlock.getPname() + "\n" +
+                    "Process   ID: " + processBlock.getPID());
         });
     }   
 }
--- JoeOS/src/joeos/ProcessManagement/Models/PCBlock.java
+++ JoeOS/src/joeos/ProcessManagement/Models/PCBlock.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 Joe.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package joeos.ProcessManagement.Models;
 
@@ -9,23 +27,57 @@ package joeos.ProcessManagement.Models;
  *
  * @author Joe
  */
-public class PCBlock {
+public class PCBlock implements Comparable<PCBlock>{
     
-    public int pID;
-    public char pState;
-    public String pName;
-    public int priority;
-    public int arrivalTime;
-    public int cpuBurst;
-    public RegisterSet RegVals;
+    private int pID;
+    private char pState;
+    private String pName;
+    private int priority;
+    private int arrivalTime;
+    private int cpuBurst;
+    private RegisterSet RegVals;
+    private PCBlock nextPCB;
     
     public PCBlock(String[] proc){
         pID = ProcessTable.getNextID();
-        pState = 'n';
+        pState = 'r';
         pName = proc[0];
         priority = Integer.parseInt(proc[1]);
         arrivalTime = Integer.parseInt(proc[2]);
         cpuBurst = Integer.parseInt(proc[3]);
         RegVals = new RegisterSet(proc);
-    }   
+    }
+    
+    public void decCpuBurst(){
+        this.cpuBurst--;
+    }
+    
+    public PCBlock nextPCB(){
+        return this.nextPCB;
+    }
+    
+    public void setNextPCB(PCBlock block){
+        this.nextPCB = block;
+    }
+    
+    public int getPID(){
+        return this.pID;
+    }
+    
+    public String getPname(){
+        return this.pName;
+    }
+
+    @Override
+    public int compareTo(PCBlock t) {
+        if(this.cpuBurst == t.cpuBurst){
+            return 0;
+        }
+        else if(this.cpuBurst > t.cpuBurst){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
 }
